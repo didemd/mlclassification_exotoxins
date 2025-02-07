62d1f47938ffac10978ab71f0a6d7de670fa3a01
didemd <didemdostt@gmail.com>
2025-02-07 03:13:02 +0100

    Basch script for all main files

--- run.sh
+++ run.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
-# Create and activate a virtual environment
+# Create and activate virtual environment
 python3 -m venv venv
 source venv/bin/activate
 
 # Install dependencies
 pip install -r requirements.txt
 
-# Run Python scripts
-python Code/python/main_type.py
+# Define script directory
+SCRIPT_DIR="Code/python"
 
+# Allow user to select a script to run
+echo "Select a script to run:"
+select script in main_target.py main_target_split.py main_target_folds_split.py main_target_nonsplit_folds.py main_type.py main_type_split.py main_type_folds_split.py "All"; do
+    case $script in
+        "All")
+            echo "Running all main scripts..."
+            for file in "$SCRIPT_DIR"/main_*.py; do
+                echo "Executing $file..."
+                python "$file"
+            done
+            break
+            ;;
+        "")
+            echo "Invalid choice. Please try again."
+            ;;
+        *)
+            echo "Running $script..."
+            python "$SCRIPT_DIR/$script"
+            break
+            ;;
+    esac
+done
 
 # Deactivate virtual environment
 deactivate
